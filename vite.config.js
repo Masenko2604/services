3b807bc25403061298aae96346c97b6066f0ad62
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2025-11-29 13:35:17 +0100

    fix base path and rebuild

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 import htmlInclude from 'vite-plugin-html-include'; 
 export default defineConfig(({ command }) => {
   return {
-    base: '/mcs9-incredibles/',
+    base: '/services/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
